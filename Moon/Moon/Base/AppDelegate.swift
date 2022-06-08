8055eac34ec0f5fc89d1ffd0c316d5420ba00a2a
sunhuaiwu <coder_sunhw@163.com>
2022-06-08 16:02:24 +0800

    add pods & img

--- Moon/Moon/Base/AppDelegate.swift
+++ Moon/Moon/Base/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+//import FBSDKCoreKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+//        ApplicationDelegate.shared.application(application,didFinishLaunchingWithOptions: launchOptions)
+//        Settings.shared.isAdvertiserTrackingEnabled = true
+//        Settings.shared.isAutoLogAppEventsEnabled = true
+//        Settings.shared.isAdvertiserIDCollectionEnabled = true
+//        Settings.shared.isCodelessDebugLogEnabled = false
+        
+//        FirebaseApp.configure()
         return true
     }
 
